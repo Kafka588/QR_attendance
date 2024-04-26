c17e672326396e604979ff220a86de564c50a1cd
Kafka588 <kafka.asagiri@gmail.com>
2024-04-26 17:23:52 +0800

    created app
    created app and added login ui

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:qr_att/loginscreen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: '<YOUR_API_KEY>',
+      appId: '<YOUR_APP_ID>',
+      messagingSenderId: '<YOUR_MESSAGING_SENDER_ID>',
+      projectId: '<YOUR_PROJECT_ID>',
+    ),
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const KeyboardVisibilityProvider(
+        child: LoginScreen(),
+      ),
+    );
+  }
+}
